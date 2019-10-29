b04790727cc1effc06e1b09a801279999eb2de89
Philip Edwards <philip@gdcorner.com>
2019-10-29 16:52:58 +1100

    Improved path building to allow more flexibility when calling test.py

--- test.py
+++ test.py
@@ -21,7 +21,11 @@ if len(sys.argv) > 1:
 device = torch.device('cpu')
 # philipedwards change end
 
-test_img_folder = 'LR/*'
+ESRGANPath = osp.realpath(__file__)
+ESRGANPath = ESRGANPath[:-len(osp.basename(ESRGANPath))]
+img_src_path = osp.join(ESRGANPath, 'LR/*')
+img_dst_path = osp.join(ESRGANPath, "results")
+print(img_src_path)
 
 model = arch.RRDBNet(3, 3, 64, 23, gc=32)
 model.load_state_dict(torch.load(model_path), strict=True)
@@ -31,7 +35,7 @@ model = model.to(device)
 print('Model path {:s}. \nTesting...'.format(model_path))
 
 idx = 0
-for path in glob.glob(test_img_folder):
+for path in glob.glob(img_src_path):
     idx += 1
     base = osp.splitext(osp.basename(path))[0]
     print(idx, base)
@@ -46,4 +50,6 @@ for path in glob.glob(test_img_folder):
         output = model(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
     output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
     output = (output * 255.0).round()
-    cv2.imwrite('results/{:s}_rlt.png'.format(base), output)
+    cv2.imwrite(img_dst_path + '/{:s}_rlt.png'.format(base), output)
+
+print("Finished ESRGAN")
